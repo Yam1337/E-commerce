ff182be50ca3f42265fa19c53c1e9c39ddb458b1
Bartek Szajna <bartek.szajna@wp.pl>
2021-05-26 19:04:31 +0200

    Further project configuration
    Added:
    	- axios
    	- styled-components
    	- React Router
    
    Fixed .src directories setup

---
+++ src/containers/HomePage/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../../components/Header';
+import MainContent from '../MainContent';
+import Footer from '../../components/Footer';
+
+const HomePage = () => {
+  return (
+    <>
+      <Header />
+      <MainContent />
+      <Footer />
+    </>
+  );
+};
+
+export default HomePage;

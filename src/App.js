d19e7ca1f08c5613fa34ee0755b26404a123b5d0
Daniel <danieldabic@wp.pl>
2021-05-28 20:07:22 +0200

    CodeReview

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
-
-// Components
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
-
-// Pages
 import HomePage from './containers/HomePage';
 import CategoriesPage from './containers/CategoriesPage';
 import CategoryPage from './containers/CategoryPage';
 import ProductPage from './containers/ProductPage';
 import ErrorPage from './containers/ErrorPage';
+import ProfilePage from './containers/ProfilePage';
 
 function App() {
   return (
@@ -19,14 +16,12 @@ function App() {
       <div className='app-container'>
         <BrowserRouter>
           <Switch>
-            <Route exact path='/' component={HomePage} />
-            <Route exact path='/home' component={HomePage} />
-            <Route exact path='/categories' component={CategoriesPage} />
-            <Route exact path='/category/:id' component={CategoryPage} />
-            <Route exact path='/product/:id' component={ProductPage} />
-            <Route exact path='/profile' component={ProductPage} />
-            <Route exact path='/404' component={ErrorPage} />
-            <Route path='*' component={HomePage} />
+            <Route exact path={['/', '/home']} component={HomePage}/>
+            <Route exact path='/categories' component={CategoriesPage}/>
+            <Route exact path='/category/:id' component={CategoryPage}/>
+            <Route exact path='/product/:id' component={ProductPage}/>
+            <Route exact path='/profile' component={ProfilePage}/>
+            <Route path='*' component={ErrorPage}/>
           </Switch>
         </BrowserRouter>
       </div>

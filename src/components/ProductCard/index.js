1d744073b33c317208df1cfc747ec869f1a08ae9
Rafal Medryk <yam1337@gmail.com>
2021-05-28 20:06:47 +0200

    product card finished

--- src/components/ProductCard/index.js
+++ src/components/ProductCard/index.js
@@ -1,5 +1,46 @@
-const ProductCard = () => {
-  return <h1>ProductCard</h1>;
-};
+import {
+  ProductCardWrapper,
+  ProductImageWrapper,
+  ProductDetailsWrapper,
+} from './styles';
+
+const products = [
+  {
+    id: '1',
+    name: 'Koszulka',
+    description: 'Zwykła koszulka o kroju basic-fit',
+    img: 'https://images.pexels.com/photos/991509/pexels-photo-991509.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260',
+    category: 'Koszulki',
+    material: '100% Bawełna',
+    adjective: 'Placeholder',
+    price: '29,99',
+    color: 'Biały',
+  },
+  {
+    id: '2',
+    name: 'Jeansy',
+    description: 'Zwykłe jeansy o kroju basic-fit',
+    img: 'https://images.pexels.com/photos/1346187/pexels-photo-1346187.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260',
+    category: 'Spodnie',
+    material: '100% Bawełna',
+    adjective: 'Placeholder',
+    price: '89,99',
+    color: 'Granatowe',
+  },
+];
+
+function ProductCard({ width, height }) {
+  return (
+    <ProductCardWrapper width={width} height={height}>
+      <ProductImageWrapper>
+        <img src={products[0].img} alt={products[0].name} />
+      </ProductImageWrapper>
+      <ProductDetailsWrapper>
+        <div>{products[0].name}</div>
+        <div>{`${products[0].price} zł`}</div>
+      </ProductDetailsWrapper>
+    </ProductCardWrapper>
+  );
+}
 
 export default ProductCard;

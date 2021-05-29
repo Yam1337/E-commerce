596f8a771ad6898b34ec3f7f7c02d8356a4410fa
MaciejP <m.ponicki1995@gmail.com>
2021-05-29 13:59:31 +0200

    integration with backend API for products

--- src/containers/HomePage/index.js
+++ src/containers/HomePage/index.js
@@ -1,5 +1,25 @@
+import { useState, useEffect } from 'react';
+import { Container, ProductCard } from './styles';
+
 const HomePage = () => {
-  return <h1>HomePage</h1>;
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('https://frontend-labs.herokuapp.com/products?_limit=10')
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+  console.log(products);
+  return (
+    <>
+      <h1>Most recent products</h1>
+      <Container>
+        {products.map((item) => (
+          <ProductCard key={item.id}>{item.image}{item.name}</ProductCard>
+        ))}
+      </Container>
+    </>
+  );
 };
 
 export default HomePage;
--- src/containers/HomePage/styles.js
+++ src/containers/HomePage/styles.js
@@ -1,1 +1,16 @@
-//  Styles
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: grid;
+  frid-column-gap: 50px;
+  grid-template-columns: 1fr 1fr;
+s  position: absolute;
+  left: 0;
+  top: 2rem;
+`;
+
+export const ProductCard = styled.div`
+  width: 100px;
+  height: 150px;
+  border: 1px solid black;
+`;

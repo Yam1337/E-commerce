2fd1923216f3332a7099cd808034cd7e9bca98f0
Bartek Szajna <bartek.szajna@wp.pl>
2021-05-31 20:25:19 +0200

    Fixing PR issues
    Switched from fetch().then()... to async/await syntax. Added dynamic
    NavLoader indicating loading process, implemented handling for fetch
    errors to prevent the loader from infinite spinning and to let the user
    know that some server issues occured.

--- src/components/NavList/index.js
+++ src/components/NavList/index.js
@@ -1,33 +1,53 @@
 import { useState, useEffect } from 'react';
-import { List, NavLinkWrapper, LinkIcon } from './styles';
+import { List, NavLinkWrapper, LinkIcon, Error } from './styles';
+import NavLoader from '../NavLoader';
 
 const NavList = ({ isMenuOpen, handleClick }) => {
   const [menuLinks, setMenuLinks] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isLoadError, setIsLoadError] = useState(false);
+
+  const fetchData = async () => {
+    const res = await fetch('https://frontend-labs.herokuapp.com/main-menu');
+    if (res.ok) {
+      const data = await res.json();
+      setMenuLinks(data.item);
+      setIsLoading(false);
+    } else {
+      setIsLoadError(true);
+      setIsLoading(false);
+    }
+  };
 
   useEffect(() => {
-    fetch('https://frontend-labs.herokuapp.com/main-menu')
-      .then((res) => res.json())
-      .then((res) => {
-        setMenuLinks(res.item);
-      });
+    fetchData().catch(() => {
+      setIsLoadError(true);
+      setIsLoading(false);
+    });
   }, []);
 
-  return (
-    <List isMenuOpen={isMenuOpen}>
-      {menuLinks.map((item) => (
-        <li key={item.id}>
-          <NavLinkWrapper
-            activeClassName='navLink'
-            to={item.path}
-            onClick={handleClick}
-          >
-            <LinkIcon className='navLink_icon' iconName={item.name} />
-            {item.name}
-          </NavLinkWrapper>
-        </li>
-      ))}
-    </List>
-  );
+  let responseToRender = '';
+
+  if (isLoading) {
+    responseToRender = <NavLoader />;
+  } else if (isLoadError) {
+    responseToRender = <Error>We couldn&apos;t fetch the data.</Error>;
+  } else {
+    responseToRender = menuLinks.map((item) => (
+      <li key={item.id}>
+        <NavLinkWrapper
+          activeClassName='navLink'
+          to={item.path}
+          onClick={handleClick}
+        >
+          <LinkIcon className='navLink_icon' iconName={item.name} />
+          {item.name}
+        </NavLinkWrapper>
+      </li>
+    ));
+  }
+
+  return <List isMenuOpen={isMenuOpen}>{responseToRender}</List>;
 };
 
 export default NavList;
--- src/components/NavList/styles.js
+++ src/components/NavList/styles.js
@@ -84,3 +84,13 @@ export const LinkIcon = styled.div`
     }
   }});
 `;
+
+export const Error = styled.div`
+  color: #989898;
+  font-size: 1.4rem;
+  font-family: 'Poppins';
+  height: 100%;
+  display: flex;
+  align-items: center;
+  text-align: center;
+`;

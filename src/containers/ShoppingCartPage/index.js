8cd4ed09eae7f5f088dfab34e7b2487a002d396d
Rafal Medryk <yam1337@gmail.com>
2021-06-07 19:36:07 +0200

    fixed horizontal mobile display

---
+++ src/containers/ShoppingCartPage/index.js
@@ -0,0 +1,7 @@
+import { ShoppingCartPageWrapper } from './styles';
+
+const ShoppingCartPage = () => {
+  return <ShoppingCartPageWrapper>Hello</ShoppingCartPageWrapper>;
+};
+
+export default ShoppingCartPage;
---
+++ src/containers/ShoppingCartPage/styles.js
@@ -0,0 +1,10 @@
+import styled, { keyframes } from 'styled-components';
+
+export const ShoppingCartPageWrapper = styled.div`
+  background-color: red;
+  height: 100%;
+`;
+
+export const ShoppingCartPageWrapper2 = styled.div`
+  background-color: red;
+`;
